c53dc82182bf0fe18eab92639e80defd450072be
Dat Trieu <d@trieu.se>
2022-12-03 10:53:27 +0100

    flatter solution for 3b

--- src/main/kotlin/3b.kt
+++ src/main/kotlin/3b.kt
@@ -4,29 +4,30 @@ import java.nio.file.Path
 fun main() {
     fun getPriority(char: Char): Int =
         if (char.isLowerCase())
-                char.code - 96
-            else
-                char.code - 38
+            char.code - 96
+        else
+            char.code - 38
 
     val lines = Files.readAllLines(Path.of("3.txt"))
 
-    val elves = mutableListOf<MutableList<MutableSet<Int>>>()
-    for (c in 0 until lines.size) {
-        val group = c/3
-        val elf = c - 3 * group
-        if (elves.elementAtOrNull(group) == null) {
-            elves.add(group, mutableListOf())
-        }
-        if (elves[group].elementAtOrNull(elf) == null) {
-            elves[group].add(mutableSetOf())
-        }
-        lines[c].forEach { elves[group][elf].add(getPriority(it)) }
-    }
+    val firstElf = mutableSetOf<Char>()
+    val secondElf = mutableSetOf<Char>()
     var res = 0L
-    elves.forEach {
-        val first = it[0]
-        val second = it[1]
-        res += it[2].filter { i -> first.contains(i) && second.contains(i) }[0]
+    lines.forEachIndexed { i, line ->
+        when (i - 3 * (i / 3)) {
+            2 -> {
+                for (c in line) {
+                    if (firstElf.contains(c) && secondElf.contains(c)) {
+                        res += getPriority(c)
+                        firstElf.clear()
+                        secondElf.clear()
+                        break
+                    }
+                }
+            }
+            1 -> line.forEach { firstElf.add(it) }
+            else -> line.forEach { secondElf.add(it) }
+        }
     }
     println(res)
 }
