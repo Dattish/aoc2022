209806ed41f2a7593b5cac52ea6e765e7ec59935
Dat Trieu <d@trieu.se>
2022-12-05 14:58:52 +0100

    stack based solutions for day 5

--- src/main/kotlin/5a.kt
+++ src/main/kotlin/5a.kt
@@ -3,61 +3,52 @@ import java.nio.file.Path
 
 fun main() {
     val lines = Files.readAllLines(Path.of("5.txt"))
-    val boxes = mutableListOf<MutableList<Char>>()
+    val boxes = mutableListOf<ArrayDeque<Char>>()
     val moveRegex = Regex("move ([0-9]+) from ([0-9]+) to ([0-9]+)")
-    lines.forEachIndexed { row, line ->
-        if (line != "") {
-            val isMove = line[0].isLetter()
-            if (!isMove) {
-                var expectedBoxes = 0
-                var i = 0
-                while (i < line.length) {
-                    val c1 = line[0 + i]
-                    val c2 = line[1 + i]
-                    if (c1 == '[') {
-                        while ((boxes.size - 1) < expectedBoxes) {
-                            boxes.add(mutableListOf())
-                        }
-                        val current = boxes.elementAtOrElse(expectedBoxes) { mutableListOf() }
-                        while ((current.size - 1) < row - 1) {
-                            current.add(' ')
-                        }
-                        current.add(row, c2)
-                        boxes[expectedBoxes] = current
+    lines.filter { it != "" }.forEach {
+        val isMove = it[0].isLetter()
+        if (!isMove) {
+            var expectedBoxes = 0
+            var i = 0
+            while (i < it.length) {
+                val c1 = it[0 + i]
+                val c2 = it[1 + i]
+                if (c1 == '[') {
+                    while ((boxes.size - 1) < expectedBoxes) {
+                        boxes.add(ArrayDeque())
                     }
-                    expectedBoxes++
-                    i += 4
+                    val current = boxes.elementAtOrElse(expectedBoxes) { ArrayDeque() }
+                    current.add(c2)
+                    boxes[expectedBoxes] = current
                 }
-            } else {
-                val matchResult = moveRegex.find(line)!!.groupValues
-                val num = matchResult[1].toInt()
-                val from = matchResult[2].toInt()
-                val to = matchResult[3].toInt()
-                val takeFrom = boxes[from - 1]
-                val giveTo = boxes[to - 1]
-                var i = 0
-                var taken = 0
-                while (taken < num && i < takeFrom.size) {
-                    val c = takeFrom.set(i, ' ')
-                    if (!c.isWhitespace()) {
-                        var indexToSet = 0
-                        while (indexToSet < giveTo.size && giveTo[indexToSet].isWhitespace()) {
-                            indexToSet++
-                        }
-                        if (indexToSet == 0) {
-                            giveTo.add(0, c)
-                        } else {
-                            giveTo[indexToSet - 1] = c
-                        }
-                        taken++
-                    }
-                    i++
+                expectedBoxes++
+                i += 4
+            }
+        } else {
+            val matchResult = moveRegex.find(it)!!.groupValues
+            val num = matchResult[1].toInt()
+            val from = matchResult[2].toInt()
+            val to = matchResult[3].toInt()
+            val takeFrom = boxes[from - 1]
+            val giveTo = boxes[to - 1]
+            var taken = 0
+            while (taken < num) {
+                val c = takeFrom.removeFirst()
+                var indexToSet = 0
+                while (indexToSet < giveTo.size && giveTo[indexToSet].isWhitespace()) {
+                    indexToSet++
                 }
+                if (indexToSet == 0) {
+                    giveTo.add(0, c)
+                } else {
+                    giveTo[indexToSet - 1] = c
+                }
+                taken++
             }
         }
     }
-    for (b in boxes.reversed()) {
-        println(b)
+    for (b in boxes) {
+        print(b.first())
     }
 }
 
--- src/main/kotlin/5b.kt
+++ src/main/kotlin/5b.kt
@@ -3,65 +3,54 @@ import java.nio.file.Path
 
 fun main() {
     val lines = Files.readAllLines(Path.of("5.txt"))
-    val boxes = mutableListOf<MutableList<Char>>()
+    val boxes = mutableListOf<ArrayDeque<Char>>()
     val moveRegex = Regex("move ([0-9]+) from ([0-9]+) to ([0-9]+)")
-    lines.forEachIndexed { row, line ->
-        if (line != "") {
-            val isMove = line[0].isLetter()
-            if (!isMove) {
-                var expectedBoxes = 0
-                var i = 0
-                while (i < line.length) {
-                    val c1 = line[0 + i]
-                    val c2 = line[1 + i]
-                    if (c1 == '[') {
-                        while ((boxes.size - 1) < expectedBoxes) {
-                            boxes.add(mutableListOf())
-                        }
-                        val current = boxes.elementAtOrElse(expectedBoxes) { mutableListOf() }
-                        while ((current.size - 1) < row - 1) {
-                            current.add(' ')
-                        }
-                        current.add(row, c2)
-                        boxes[expectedBoxes] = current
+    lines.filter { it != "" }.forEach {
+        val isMove = it[0].isLetter()
+        if (!isMove) {
+            var i = 0
+            while (i < it.length) {
+                val expectedBoxes = i / 4
+                val c1 = it[0 + i]
+                val c2 = it[1 + i]
+                if (c1 == '[') {
+                    while ((boxes.size - 1) < expectedBoxes) {
+                        boxes.add(ArrayDeque())
                     }
-                    expectedBoxes++
-                    i += 4
+                    val current = boxes.elementAtOrElse(expectedBoxes) { ArrayDeque() }
+                    current.add(c2)
+                    boxes[expectedBoxes] = current
                 }
-            } else {
-                val matchResult = moveRegex.find(line)!!.groupValues
-                val num = matchResult[1].toInt()
-                val from = matchResult[2].toInt()
-                val to = matchResult[3].toInt()
-                val takeFrom = boxes[from - 1]
-                val giveTo = boxes[to - 1]
-                var i = 0
-                var taken = 0
-                val toAdd = mutableListOf<Char>()
-                while (taken < num) {
-                    val c = takeFrom.set(i, ' ')
-                    if (!c.isWhitespace()) {
-                        toAdd.add(c)
-                        taken++
-                    }
-                    i++
+                i += 4
+            }
+        } else {
+            val matchResult = moveRegex.find(it)!!.groupValues
+            val num = matchResult[1].toInt()
+            val from = matchResult[2].toInt()
+            val to = matchResult[3].toInt()
+            val takeFrom = boxes[from - 1]
+            val giveTo = boxes[to - 1]
+            var taken = 0
+            val toAdd = mutableListOf<Char>()
+            while (taken < num) {
+                toAdd.add(takeFrom.removeFirst())
+                taken++
+            }
+            while (toAdd.isNotEmpty()) {
+                var indexToSet = 0
+                while (indexToSet < giveTo.size && giveTo[indexToSet].isWhitespace()) {
+                    indexToSet++
                 }
-                while (toAdd.isNotEmpty()) {
-                    var indexToSet = 0
-                    while (indexToSet < giveTo.size && giveTo[indexToSet].isWhitespace()) {
-                        indexToSet++
-                    }
-                    if (indexToSet == 0) {
-                        giveTo.add(0, toAdd.removeLast())
-                    } else {
-                        giveTo[indexToSet - 1] = toAdd.removeLast()
-                    }
+                if (indexToSet == 0) {
+                    giveTo.add(0, toAdd.removeLast())
+                } else {
+                    giveTo[indexToSet - 1] = toAdd.removeLast()
                 }
             }
         }
     }
-    for (b in boxes.reversed()) {
-        println(b)
+    for (b in boxes) {
+        print(b.first())
     }
 }
 
