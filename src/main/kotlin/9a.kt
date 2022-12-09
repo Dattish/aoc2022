cdb7d152419d20f1903e91b255466ae15ee764ee
Dat Trieu <d@trieu.se>
2022-12-09 11:23:27 +0100

    solutions for day 9

---
+++ src/main/kotlin/9a.kt
@@ -0,0 +1,126 @@
+import java.io.File
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+fun main() {
+    val lines = File("9.txt").readLines()
+
+    data class Coords(var x: Long, var y: Long)
+
+    fun handleYMove(hPos: Coords, tPos: Coords, followMove: () -> Boolean) {
+        val yDiff = (hPos.y - tPos.y).absoluteValue
+        if (yDiff > 1 && tPos.x != hPos.x) {
+            val xDiff = hPos.x - tPos.x
+            when (xDiff.sign) {
+                1 -> {
+                    tPos.x++
+                    followMove()
+                }
+
+                -1 -> {
+                    tPos.x--
+                    followMove()
+                }
+            }
+        }
+    }
+
+    fun handleXMove(hPos: Coords, tPos: Coords, followMove: () -> Boolean) {
+        val xDiff = (hPos.x - tPos.x).absoluteValue
+        if (xDiff > 1 && tPos.y != hPos.y) {
+            val yDiff = hPos.y - tPos.y
+            when (yDiff.sign) {
+                1 -> {
+                    tPos.y++
+                    followMove()
+                }
+
+                -1 -> {
+                    tPos.y--
+                    followMove()
+                }
+            }
+        }
+    }
+
+    val tPos = Coords(0, 0)
+    val hPos = Coords(0, 0)
+    val allTPos = mutableSetOf(tPos.copy())
+
+    for (line in lines) {
+        val m = line.split(" ")
+        val direction = m[0]
+        var hMoves = m[1].toInt()
+
+        when (direction) {
+            "R" -> {
+                val followMove = {
+                    tPos.x++
+                    allTPos.add(tPos.copy())
+                }
+                while (hMoves > 0) {
+                    hMoves--
+                    hPos.x++
+                    handleXMove(hPos, tPos, followMove)
+                    val xDiff = (hPos.x - tPos.x).absoluteValue
+                    val yDiff = (hPos.y - tPos.y).absoluteValue
+                    if (xDiff > 1 || yDiff > 1) {
+                        followMove()
+                    }
+                }
+            }
+
+            "L" -> {
+                val followMove = {
+                    tPos.x--
+                    allTPos.add(tPos.copy())
+                }
+                while (hMoves > 0) {
+                    hMoves--
+                    hPos.x--
+                    handleXMove(hPos, tPos, followMove)
+                    val xDiff = (hPos.x - tPos.x).absoluteValue
+                    val yDiff = (hPos.y - tPos.y).absoluteValue
+                    if (xDiff > 1 || yDiff > 1) {
+                        followMove()
+                    }
+                }
+            }
+
+            "U" -> {
+                val followMove = {
+                    tPos.y++
+                    allTPos.add(tPos.copy())
+                }
+                while (hMoves > 0) {
+                    hMoves--
+                    hPos.y++
+                    handleYMove(hPos, tPos, followMove)
+                    val xDiff = (hPos.x - tPos.x).absoluteValue
+                    val yDiff = (hPos.y - tPos.y).absoluteValue
+                    if (xDiff > 1 || yDiff > 1) {
+                        followMove()
+                    }
+                }
+            }
+
+            else -> {
+                val followMove = {
+                    tPos.y--
+                    allTPos.add(tPos.copy())
+                }
+                while (hMoves > 0) {
+                    hMoves--
+                    hPos.y--
+                    handleYMove(hPos, tPos, followMove)
+                    val xDiff = (hPos.x - tPos.x).absoluteValue
+                    val yDiff = (hPos.y - tPos.y).absoluteValue
+                    if (xDiff > 1 || yDiff > 1) {
+                        followMove()
+                    }
+                }
+            }
+        }
+    }
+    println(allTPos.size)
+}
---
+++ src/main/kotlin/9b.kt
@@ -0,0 +1,51 @@
+
+import java.io.File
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+fun main() {
+    val lines = File("9.txt").readLines()
+
+    data class Coords(var x: Int, var y: Int)
+
+
+    fun shouldMove(hPos: Coords, tPos: Coords): Boolean {
+        val yDiff = (hPos.y - tPos.y).absoluteValue
+        val xDiff = (hPos.x - tPos.x).absoluteValue
+        return yDiff > 1 || xDiff > 1
+    }
+
+    fun getMoves(hPos: Coords, tPos: Coords): Coords {
+        return Coords((hPos.x-tPos.x).sign, (hPos.y-tPos.y).sign)
+    }
+
+    val positions = Array(10) { Coords(0, 0) }
+    val allTPos = mutableSetOf(positions.last().copy())
+
+    for (line in lines) {
+        val m = line.split(" ")
+        val direction = m[0]
+        for (i in 0 until m[1].toInt()) {
+            when (direction) {
+                "R" -> positions[0].x++
+                "L" -> positions[0].x--
+                "U" -> positions[0].y++
+                "D" -> positions[0].y--
+                else -> throw RuntimeException(direction)
+            }
+            for (j in 1 until positions.size) {
+                val hPos = positions[j - 1]
+                val tPos = positions[j]
+                if (shouldMove(hPos, tPos)) {
+                    val move = getMoves(hPos, tPos)
+                    tPos.x += move.x
+                    tPos.y += move.y
+                    if (j == positions.size - 1) {
+                        allTPos.add(tPos.copy())
+                    }
+                }
+            }
+        }
+    }
+    println(allTPos.size)
+}
